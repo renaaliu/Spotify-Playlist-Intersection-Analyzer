5e3936fdb281e802618cf94f7af1e40a2120f7ac
Rena Liu <renaliu@Renas-MacBook-Air.local>
2019-06-12 18:49:32 -0400

    didn't work this time

--- testingAPI.py
+++ testingAPI.py
@@ -84,18 +84,13 @@ import spotipy.util as util
 from json.decoder import JSONDecodeError
 # Get the username from terminal
 username = sys.argv[1]
-#scope = 'user-read-private user-read-playback-state user-modify-playback-state'
-
-# spotify:user:renal1912
-
+scope = 'user-read-private user-read-playback-state user-modify-playback-state'
 # Erase cache and prompt for user permission
 try:
-    # os.remove(f".cache-{username}")
     token = util.prompt_for_user_token(username)
     #, scope) # add scope
-except: 
-#(AttributeError, JSONDecodeError):
-    os.remove(f"~/.cache-{username}")
+except (AttributeError, JSONDecodeError):
+    os.remove('.cache-{}'.format(username))
     token = util.prompt_for_user_token(username)
     #, scope) # add scope
 # Create our spotify object with permissions
